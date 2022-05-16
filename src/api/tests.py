68ea4e6cd3b1244117ae43275335896b911a9b2a
Oguzhan Akan <oakan13@ku.edu.tr>
2022-05-16 11:22:34 -0700

    added tests

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Create your tests here.
+
+from labtest.models import Location
+
+
+class LocationTestCase(TestCase):
+    def setUp(self):
+        Location.objects.create(name="CovidLab San Diego",
+                                slug="sandiego", longitude=1, latitude=1,
+                                email="sandiego@covidlab.com")
+
+    def test_location_string(self):
+        loc = Location.objects.get(slug="sandiego")
+        self.assertEqual(str(loc), 'sandiego')
